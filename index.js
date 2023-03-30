5857eaa9bbfc5bc04c8bcbd6fed4aeca5180645e
thilinaRp <chaturanga.rpt@gmail.com>
2023-03-30 18:28:50 +0530

    remove args

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ app.get("/", async (req, res) => {
 
 app.get("/getposts/:pageId", async (req, res) => {
   const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
